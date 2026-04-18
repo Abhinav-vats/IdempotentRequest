9379267436ed50d1b1bcb3e16aca576ce371d226
Abhinav-vats <50848068+Abhinav-vats@users.noreply.github.com>
2026-04-18 21:57:47 +0530

    main push

---
+++ src/main/java/com/abhinav/Idempotency/IdempotencyApplication.java
@@ -0,0 +1,13 @@
+package com.abhinav.Idemptency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdemptencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdemptencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhinav/Idempotency/IdempotencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinav.Idemptency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdemptencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
